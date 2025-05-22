cff1ba65b2ed53dbd74882a81b6fb385dad16c4a
mrrobot <stivomoses001@gmail.com>
2025-05-22 15:56:50 +0300

    design welcome message and user input field

---
+++ chat/static/chat/index.js
@@ -0,0 +1,46 @@
+/** This file contains the JavaScript code for the chatbot functionality. **/
+
+// welcome subtext animation
+const messages = [
+    `
+      <span><i class="fas fa-user-secret"></i> Anonymous</span>
+      <span class="separator"></span>
+      <span><i class="fas fa-dollar-sign"></i> Free</span>
+      <span class="separator"></span>
+      <span><i class="fas fa-lock"></i> Secure</span>
+    `,
+    `
+    <div><i class="fas fa-calendar-check"></i> <a href="#">Book professional appointment</a> – <strong>Ksh 4500</strong></div>    
+    `
+  ];
+  
+let currentIndex = 0;
+const subtextContainer = document.getElementById('subtextContainer');
+  
+function rotateSubtext() {
+    subtextContainer.classList.remove('active');
+    subtextContainer.classList.add('exit');
+  
+    setTimeout(() => {
+      currentIndex = (currentIndex + 1) % messages.length;
+      subtextContainer.innerHTML = messages[currentIndex];
+      subtextContainer.classList.remove('exit');
+      subtextContainer.classList.add('active');
+    }, 500); // wait for exit animation
+}
+  
+setInterval(rotateSubtext, 3500);
+  
+
+// chat input
+const textarea = document.getElementById('userInput');
+const sendButton = document.getElementById('sendButton');
+
+textarea.focus();
+
+textarea.addEventListener('input', () => {
+    textarea.style.height = 'auto'; // Reset
+    textarea.style.height = textarea.scrollHeight + 'px'; // Adjust
+
+    sendButton.disabled = textarea.value.trim() === '';
+});
