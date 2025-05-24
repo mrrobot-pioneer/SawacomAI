9972138086c79359af2f42d9faf36cec46dc7133
mrrobot <stivomoses001@gmail.com>
2025-05-25 02:43:59 +0300

    add user authentication views, templates, and styles for login and signup

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    """
+    Render the login page.
+    """
+    return render(request, 'users/login.html')
+
+def signup(request):
+    """
+    Render the registration page.
+    """
+    return render(request, 'users/signup.html')
+
+def logout(request):
+    """
+    Render the logout page.
+    """
+    return render(request, 'users/logout.html')
